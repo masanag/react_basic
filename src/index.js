0c634a97d52c14c698604d39e30a95eb7c08e4d8
masanag <masaki.nagaishi@gmail.com>
2017-12-31 16:31:53 +0900

    Section1

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// function Greeting(props) {
+//   return <h1>Hello, {props.name}</h1>;
+// }
+
+class Greeting extends React.Component {
+  render() {
+    return <h1>Hello, {this.props.name}!!</h1>
+  }
+}
+
+const element = <Greeting name="Tom"/>
+
+ReactDOM.render(
+  element,
+  document.getElementById('root')
+);
