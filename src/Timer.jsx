1db4b87fdaf3a24a11094e2be832ea6792621e2d
masanag <masaki.nagaishi@gmail.com>
2018-01-01 09:20:00 +0900

    Timer

---
+++ src/Timer.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+export default class Timer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {remaining: this.props.seconds};
+  }
+
+  countDown() {
+    if(this.state.remaining > 0) {
+      this.setState((prevState) => ({
+        remaining : prevState.remaining - 1
+      }));
+    }
+  }
+
+  componentDidMount() {
+    this.interval = setInterval(() => this.countDown(), 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello, {this.props.name}</h1>
+        <h2>{this.state.remaining} seconds remaining.</h2>
+      </div>
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,17 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './index.css';
+import Timer from './Timer.jsx';
 
-// function Greeting(props) {
-//   return <h1>Hello, {props.name}</h1>;
-// }
-
-class Greeting extends React.Component {
-  render() {
-    return <h1>Hello, {this.props.name}!!</h1>
-  }
-}
-
-const element = <Greeting name="Tom"/>
+const element = <Timer name="Filange" seconds={180} array={[1,2,3,4,5]} />;
 
 ReactDOM.render(
   element,
